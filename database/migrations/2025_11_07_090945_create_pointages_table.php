864785850088cfa0c1b5ef4c588c05154ffbdec1
Yacouba227 <halidouyacouba999@gmail.com>
2025-11-07 11:13:47 +0100

    Added a other table

---
+++ database/migrations/2025_11_07_090945_create_pointages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pointages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('statut');
+            $table->string('justification');
+            $table->date('date');
+            $table->date('date_arrive');
+            $table->date('date_depart');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pointages');
+
+    }
+};
